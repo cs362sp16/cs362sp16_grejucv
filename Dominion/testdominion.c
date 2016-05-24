e9469c09d686f053aef1c0e49c09f88b6fcc02a9
grejucv <vgrejuc@Vasiles-MacBook-Pro.local>
2016-05-23 20:36:06 -0700

    testdominion.c added and makefile changed

---
+++ Dominion/testdominion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <string.h>
+#include "interface.h"
+#include "dominion.h"
+
+int main(){
+    printf("Testing Dominion\n");
+    
+    struct gameState G;
+    int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse,
+	       sea_hag, tribute, smithy};
+    int result;
+    
+    result = initializeGame(5, k, 3, &G);
+
+}
